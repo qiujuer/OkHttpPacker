8670b0d810dcde1a5f070f30bf0aa61ebb188bcc
Qiujuer <qiujuer@live.cn>
2016-10-23 17:30:58 +0800

    Change all file License

--- okhttp/src/main/java/net/qiujuer/common/okhttp/DefaultResolver.java
+++ okhttp/src/main/java/net/qiujuer/common/okhttp/DefaultResolver.java
@@ -1,9 +1,6 @@
 /*
- * Copyright (C) 2016 Qiujuer <qiujuer@live.cn>
+ * Copyright (C) 2014-2016 Qiujuer <qiujuer@live.cn>
  * WebSite http://www.qiujuer.net
- * Created 1/1/2016
- * Changed 1/1/2016
- * Version 1.0.0
  * Author Qiujuer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
